9376de3eeadd862034909199180a36eee7106019
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-31 22:23:31 +0200

    Create Esercizio 3 – testa o croce in array paralleli.java

---
+++ 10.Verifica Java iterazione e array ( Mar 17 )/Esercizio 3 – testa o croce in array paralleli.java
@@ -0,0 +1,49 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int[] moneto1 = new int[100];
+		int[] moneto2 = new int[100];
+		int[] moneto3 = new int[100];
+		
+		int countTesta = 0;
+		int countCroce = 0;
+		
+		int[] countCombinazioni = new int[4]; //i[0]:3 teste e 0 croci , i[1]:2 teste 1 croce, i[3]:1 testa 2 croci , i[4]:3 croci
+		int prevCombinazione = 0;
+		
+		int consecutiviCombinazioni = 0;
+		
+		for(int i = 0; i < 100; i++){
+		    int[] lancia = new int[3];
+		    
+		    for(int j = 0; j < 3; j++){
+		        lancia[j] = (int) (Math.random() * 2);
+		    }
+		    
+		    int testa = 0;
+		    
+		    moneto1[i] = lancia[0]; moneto2[i] = lancia[1]; moneto3[i] = lancia[2];
+		    
+		    for(int k = 0; k < 3; k++){
+		        if (lancia[k] == 0) { countTesta++; testa++; } else countCroce++;
+		    }
+		    
+		    countCombinazioni[3 - testa]++;
+		    
+		    if(i != 0 && 3 - testa == prevCombinazione) consecutiviCombinazioni++;
+		    
+		    prevCombinazione = 3 - testa;
+		}
+		
+		System.out.printf("Teste: %d%n", countTesta);
+		System.out.printf("Croce: %d%n%n", countCroce);
+		System.out.printf("Due lanci consecutivi la stessa combinazione: %d%n", consecutiviCombinazioni);
+		
+		String[] combinazioniNome = {"3 teste e 0 croci", "2 teste e 1 croce", "1 testa e 2 croci", "3 croci"};
+		
+		System.out.printf("%nCombinazioni: %n");
+		for(int i = 0; i < 4; i++){
+		    System.out.printf("%s: %d%n", combinazioniNome[i], countCombinazioni[i]);
+		}
+	}
+}
