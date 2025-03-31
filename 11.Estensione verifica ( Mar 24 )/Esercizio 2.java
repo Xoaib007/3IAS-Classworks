dd0d9a41bafc2ceae49510fd19dc6dc3cd0ef004
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-31 22:25:01 +0200

    Create Esercizio 2.java

---
+++ 11.Estensione verifica ( Mar 24 )/Esercizio 2.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int[] numeriCasuali = new int[100];
+
+		int countDispari = 0;
+		int countPari = 0;
+		int countDivisore1 = 0;
+		int countDivisore2 = 0;
+
+
+		for(int i = 0; i < 100; i++) {
+			int numero = (int)(Math.random() * 100);
+			numeriCasuali[i] = numero;
+
+			if(numero % 2 == 0) {
+				countPari++;
+			} else {
+				countDispari++;
+			}
+
+			if (i != 0 && numeriCasuali[i - 1] != 0 && numeriCasuali[i] % numeriCasuali[i - 1] == 0) {
+                countDivisore++;
+            }
+            
+            if (i != 0 && numero != 0 && numeriCasuali[i - 1] % numero == 0) {
+                countDivisore2++;
+            }
+		}
+
+		for(int num: numeriCasuali) {
+			System.out.printf("%d, ", num);
+		}
+		System.out.printf("%n");
+
+		System.out.printf("Dispari: %d%n", countDispari);
+		System.out.printf("Pari: %d%n", countPari);
+		System.out.printf("Divisore della posizione i-1: %d%n", countDivisore);
+		System.out.printf("Divisore della posizione i: %d%n", countDivisore2);
+	}
+}
