903a0a83b9007d1ee64bd596990d18342f36f680
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2025-03-31 22:35:58 +0200

    Mar 31

--- 13.Esercitazione in laboratorio con i metodi statici ( Mar 31 )/Termp Converter.java
+++ 13.Esercitazione in laboratorio con i metodi statici ( Mar 31 )/Termp Converter.java
@@ -24,8 +24,9 @@ public class Main
 	public static double celsiusToFarenheit(double celsius) {
         return (celsius * 9 / 5) + 32;
     }
-    
+	
     public static double farenheitToCelsius(double farenheit) {
         return (farenheit - 32) * 5 / 9;
     }
 }
+
